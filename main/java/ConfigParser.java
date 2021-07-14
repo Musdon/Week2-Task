c8bbdd6b567781a63f64fb415c977a89b81c72df
Musdon <majibade5@gmail.com>
2021-07-14 13:28:06 +0100

    ConfigParser Task

---
+++ main/java/ConfigParser.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ConfigParser {
+
+    private String fileName;
+    private Map<String, String> check;
+
+
+    public ConfigParser(String fileName) {
+        this.check= new HashMap<String, String>();
+        if(fileName.equalsIgnoreCase("production")){
+            this.fileName = "/Users/Decagon/IdeaProjects/ConfigParser/src/main/java/config.txt";
+        }else if(fileName.equalsIgnoreCase("development")){
+            this.fileName = "/Users/Decagon/IdeaProjects/ConfigParser/src/main/java/config.txt.dev";
+        }else if(fileName.equalsIgnoreCase("staging")) {
+            this.fileName = "/Users/Decagon/IdeaProjects/ConfigParser/src/main/java/config.txt.staging";
+        }
+
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    private Map<String, String> getCheck() {
+        return check;
+    }
+
+    public String get(String key){
+        return getCheck().get(key);
+    }
+
+    public void ReadWithInputStream() {
+        String line;
+        FileReader fin = null;
+        BufferedReader br = null;
+        try {
+            fin = new FileReader(this.fileName);
+            br = new BufferedReader(fin);
+            String prefix = "";
+            while ((line = br.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    if (line.startsWith("[") && line.endsWith("]")) {
+                        prefix = line.substring(1, line.length() - 1);
+                        continue;
+                    }
+                    String[] keyValue = line.split("=");
+                    if(prefix.isEmpty()) {
+                        String key = keyValue[0];
+                        String value = keyValue[1];
+                        if(!this.check.containsKey(key)){
+                            this.check.put(key, value);
+                        }
+                    }else {
+                        String key = prefix + "."+ keyValue[0];
+                        String value = keyValue[1];
+                        if(!this.check.containsKey(key)){
+                            this.check.put(key, value);
+                        }
+                    }
+                }else{
+                    prefix ="";
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        } finally {
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (Exception e) {
+                System.err.println("Exception: " + e.getMessage());
+            }
+        }
+        System.out.println(this.check);
+    }
+
+    public static void main(String[] args) {
+        String name;
+        if(args.length == 0){
+            name = "production";
+        }else{
+            name = args[0];
+        }
+        ConfigParser config = new ConfigParser(name);
+        config.ReadWithInputStream();
+        System.out.println(config.get("application.name"));
+    }
+}
---
+++ main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+        public static void main(String[] args) {
+            ConfigParser config = new ConfigParser("production");
+       config.ReadWithInputStream();
+        }
+}
